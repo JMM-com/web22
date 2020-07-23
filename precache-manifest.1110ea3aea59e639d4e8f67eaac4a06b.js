310b0d3bd2aee45c355158c8b430b8d0cb22a226
JMM-com <jalalmarzooq@gmail.com>
2020-07-23 14:22:05 +0400

    Updates

---
+++ precache-manifest.1110ea3aea59e639d4e8f67eaac4a06b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad73ec86975e94ac40b10ce72dfa5fd",
+    "url": "/web22/index.html"
+  },
+  {
+    "revision": "aef70aa08b699b60050a",
+    "url": "/web22/static/css/main.3019897f.chunk.css"
+  },
+  {
+    "revision": "4943669edaa3a77110b2",
+    "url": "/web22/static/js/2.a1c6bf20.chunk.js"
+  },
+  {
+    "revision": "aef70aa08b699b60050a",
+    "url": "/web22/static/js/main.46dc5b54.chunk.js"
+  },
+  {
+    "revision": "2293adeadea606e73aea",
+    "url": "/web22/static/js/runtime-main.0bfba731.js"
+  }
+]);
